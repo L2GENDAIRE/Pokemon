867b6aa8bf3e9e2ce548cf5d399a7d41cbc54b81
L2GENDAIRE <leodoye01@gmail.com>
2026-03-18 13:16:01 +0100

    Premier commit de mon projet

---
+++ src/pokemons/PokType.java
@@ -0,0 +1,25 @@
+package pokemons;
+
+public enum PokType {
+	ACIER,
+	BIRD,
+	COMBAT,
+	DRAGON,
+	EAU,
+	ELECTRIK,
+	FEE,
+	FEU,
+	GLACE,
+	inconnu,
+	INSECTE,
+	NORMAL,
+	OBSCUR,
+	PLANTE,
+	POISON,
+	PSY,
+	ROCHE,
+	SOL,
+	SPECTRE,
+	TENEBRES,
+	VOL;	
+}
---
+++ src/pokemons/Pokemon.java
@@ -0,0 +1,11 @@
+package pokemons;
+
+public abstract class Pokemon {
+	private String nom;
+	private double taille;
+	private double poids;
+	private int pv;
+	private PokType type;
+	
+
+}
